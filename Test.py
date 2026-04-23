bbbc4e020e93c92febacdcb843ebca75a402c97f
你的用户名 <3257295075@qq.com>
2026-04-23 14:49:45 +0800

    Add GUI-based polyp quantification system

--- Test.py
+++ Test.py
@@ -4,39 +4,51 @@ import numpy as np
 import os, argparse
 from scipy import misc
 from lib.pvt import PolypPVT
-from utils.dataloader import test_dataset
 import cv2
+from PIL import Image
+import torchvision.transforms as transforms
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--testsize', type=int, default=352, help='testing size')
-    parser.add_argument('--pth_path', type=str, default='./model_pth/PolypPVT.pth')
+    parser.add_argument('--pth_path', type=str, default=r"C:\Users\Administrator\Desktop\ploy2026\Polyp-PVT\PolypPVT.pth")
     opt = parser.parse_args()
+
+    device = torch.device('cpu')
+
     model = PolypPVT()
-    model.load_state_dict(torch.load(opt.pth_path))
-    model.cuda()
+    model.load_state_dict(torch.load(opt.pth_path, map_location=device))
+    model.to(device)
     model.eval()
-    for _data_name in ['CVC-300', 'CVC-ClinicDB', 'Kvasir', 'CVC-ColonDB', 'ETIS-LaribPolypDB']:
 
-        ##### put data_path here #####
-        data_path = './dataset/TestDataset/{}'.format(_data_name)
-        ##### save_path #####
-        save_path = './result_map/PolypPVT/{}/'.format(_data_name)
+    image_root = r"D:\desktop\202508\消化道息肉检测\train\images"
+    save_path = r"C:\Users\Administrator\Desktop\ploy2026\Polyp-PVT\test_ploy"
 
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        image_root = '{}/images/'.format(data_path)
-        gt_root = '{}/masks/'.format(data_path)
-        num1 = len(os.listdir(gt_root))
-        test_loader = test_dataset(image_root, gt_root, 352)
-        for i in range(num1):
-            image, gt, name = test_loader.load_data()
-            gt = np.asarray(gt, np.float32)
-            gt /= (gt.max() + 1e-8)
-            image = image.cuda()
-            P1,P2 = model(image)
-            res = F.upsample(P1+P2, size=gt.shape, mode='bilinear', align_corners=False)
-            res = res.sigmoid().data.cpu().numpy().squeeze()
-            res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-            cv2.imwrite(save_path+name, res*255)
-        print(_data_name, 'Finish!')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    image_list = [f for f in os.listdir(image_root) if f.lower().endswith(('.jpg', '.png'))]
+
+    transform = transforms.Compose([
+        transforms.Resize((opt.testsize, opt.testsize)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+    for name in image_list:
+        image_path = os.path.join(image_root, name)
+        with open(image_path, 'rb') as f:
+            img = Image.open(f).convert('RGB')
+        h, w = img.size[1], img.size[0]
+        image = transform(img).unsqueeze(0).to(device)
+        P1, P2 = model(image)
+        res = F.upsample(P1 + P2, size=(h, w), mode='bilinear', align_corners=False)
+        res = res.sigmoid().data.cpu().numpy().squeeze()
+        res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+        if name.lower().endswith('.jpg'):
+            save_name = os.path.splitext(name)[0] + '.png'
+        else:
+            save_name = name
+        cv2.imwrite(os.path.join(save_path, save_name), res * 255)
+    print('Finish!')
--- lib/pvt.py
+++ lib/pvt.py
@@ -160,7 +160,7 @@ class PolypPVT(nn.Module):
         super(PolypPVT, self).__init__()
 
         self.backbone = pvt_v2_b2()  # [64, 128, 320, 512]
-        path = './pretrained_pth/pvt_v2_b2.pth'
+        path =r"C:\Users\Administrator\Desktop\ploy2026\Polyp-PVT\pvt_v2_b2.pth"
         save_model = torch.load(path)
         model_dict = self.backbone.state_dict()
         state_dict = {k: v for k, v in save_model.items() if k in model_dict.keys()}
